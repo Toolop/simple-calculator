6f08777302f52da7febb3facef80ff788b9782f5
Ahmad Jailani <32035123+ajailani4@users.noreply.github.com>
2018-11-25 17:41:01 +0700

    (Y/N) feature

--- calculator.py
+++ calculator.py
@@ -1,7 +1,20 @@
 print("Welcome")
-print()
 
-a = int(input("Input value of A = "))
-b = int(input("Input value of B = "))
-c = a + b
-print("A + B = ", c)
+run = True
+
+while run: 
+  print()
+  a = int(input("Input value of A = "))
+  b = int(input("Input value of B = "))
+  c = a + b
+  print("A + B = ", c)
+  
+  ans = input("Want to this again? (Y/N) ").upper()
+  
+  if ans == "Y":
+    run = True
+  elif ans == "N":
+    run = False
+
+print()
+print("Bye")
